5ca578d11d95680ecf51252140b7ce0c85d0c88b
1648moon <97297825+1648moon@users.noreply.github.com>
2024-02-03 16:36:40 +0900

    add screenshot

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Momentum 
+My Own Momtentum
 
-Here we go!
+<img src="img/screenshot.png">
+
+I've made my own momentum. Here you can get a greeting with the name you typed in, and check the weather right away where you're located. Also, you can see a different quote every time, and write and check out what you're going to do today.
--- js/background.js
+++ js/background.js
@@ -1,19 +1,17 @@
 const images = [
-    "0.JPG",
-    "1.JPG",
-    "2.JPG",
+  "0.JPG",
+  "1.JPG",
+  "2.JPG",
+  "3.JPG",
+  "4.JPG",
+  "5.JPG",
+  "6.JPG",
+  "7.JPG",
+  "8.JPG",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-// const image = document.createElement("img");
-// image.src = `img/${chosenImage}`;
 let imageUrl = `url(img/${chosenImage})`;
-
-//document.body.appendChild(image);
 
 document.body.style.backgroundImage = imageUrl;
 document.body.style.backgroundSize = "cover";
-
-
-
